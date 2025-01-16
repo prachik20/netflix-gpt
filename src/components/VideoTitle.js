46bf444ef1549e510ce4b3bf3c748a58b1ff655f
prachik20 <prachi.kavade6@gmail.com>
2025-01-16 16:30:35 +0100

    changed the favicon

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="w-screen aspect-video pt-48 px-14 text-white absolute bg-gradient-to-r from-[rgb(0_0_0/.5)_30%]">
-      <h1 className="text-4xl font-bold ">{title}</h1>
-      <p className="pt-8 w-2/4 ">{overview}</p>
+    <div className="w-screen aspect-video pt-[25%] md:pt-64 px-6 md:px-14 text-white absolute bg-gradient-to-r from-[rgb(0_0_0/.5)_30%]">
+      <h1 className="text-2xl md:text-4xl font-bold ">{title}</h1>
+      <p className="hidden md:inline-block pt-8 w-2/4 ">{overview}</p>
       <div>
-        <button className="bg-white text-black my-8 py-2 px-10 rounded-md hover:opacity-80">
+        <button className="bg-white text-black my-2 md:my-0 py-1 md:py-2 px-4 md:px-10 rounded-md hover:opacity-80">
           Play
         </button>
-        <button className="bg-gray-600 my-8 ml-2 py-2 px-8 rounded-md opacity-70 hover:opacity-50">
+        <button className="hidden md:inline-block bg-gray-600 my-8 ml-2 py-2 px-8 rounded-md opacity-70 hover:opacity-50">
           More Info
         </button>
       </div>
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,20 +4,23 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    popularMovies: null,
     movieTrailer: null,
+    topratedMovies: null,
+    upcomingMovies: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
-    addPopularMovies:(state,action)=>{
-      state.popularMovies= action.payload;
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
     },
-    addTopRatedMovies:(state,action)=>{
-      state.topRatedMovies= action.payload;
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
     },
-    addUpcomingMovies:(state,action)=>{
-      state.upComingMovies= action.payload;
+    addUpcomingMovies: (state, action) => {
+      state.upComingMovies = action.payload;
     },
     addMovieTrailer: (state, action) => {
       state.movieTrailer = action.payload;
@@ -25,5 +28,11 @@ const moviesSlice = createSlice({
   },
 });
 
-export const { addNowPlayingMovies, addMovieTrailer,addPopularMovies,addTopRatedMovies ,addUpcomingMovies} = moviesSlice.actions;
+export const {
+  addNowPlayingMovies,
+  addMovieTrailer,
+  addPopularMovies,
+  addTopRatedMovies,
+  addUpcomingMovies,
+} = moviesSlice.actions;
 export default moviesSlice.reducer;
